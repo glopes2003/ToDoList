749454ecfdc0453bb26038608272835180c8fa37
Giovana Lopes <112590544+glopes2003@users.noreply.github.com>
2023-05-20 19:14:48 -0300

    first commit

---
+++ components/todo.js
@@ -0,0 +1,21 @@
+/* Here we are displaying the added things to do */
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({task, toggleComplete}) => {
+  return (
+    <div className='Todo'>
+      <p onClick={() => toggleComplete(task.id)} className= {`${task.completed ? 'completed' : ""}`} >{task.task}</p>     {/* to mark when completed */}
+      <div>
+        <FontAwesomeIcon icon = {faPenToSquare} />
+        <FontAwesomeIcon icon = {faTrash} />
+      </div>
+
+      
+
+    </div>
+  )
+}
