e9bd62925a4324a8a27b4aadcbfc83ed1c0b7754
GLopes <gicaalopes2003@gmail.com>
2023-05-20 21:56:35 -0300

    changing the default value

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const EditTodoForm = ({editTodo, task}) => {
-    const [value, setValue] = useState(" ");  
+    const [value, setValue] = useState(task.task);  
     const handleSubmit = e => {
         
 
