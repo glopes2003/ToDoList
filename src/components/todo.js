cdab1de65a56c0f32fbbe578e84b6523829d506e
GLopes <gicaalopes2003@gmail.com>
2023-05-20 21:28:18 -0300

    exporting the edit and delete props

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task, toggleComplete}) => {
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
       <p onClick={() => toggleComplete(task.id)} className= {`${task.completed ? 'completed' : ""}`} >{task.task}</p>     {/* to mark when completed */}
